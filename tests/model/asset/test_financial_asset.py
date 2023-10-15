b515c571bf66a176192e6786230d4142ca8c13d8
bossenti <bossenti@posteo.de>
2023-10-15 12:56:00 +0200

    refactor: improve unit tests
    Signed-off-by: bossenti <bossenti@posteo.de>

--- tests/model/asset/test_financial_asset.py
+++ tests/model/asset/test_financial_asset.py
@@ -4,7 +4,7 @@ from unittest.mock import MagicMock, patch
 
 from requests import Response
 
-from tests.test_utils.requests_utils import mock_api_call
+from tests.test_utils.requests_utils import mock_api_call, EXPECTED_PRICE_DATA_LAST, EXPECTED_PRICE_DATA_CURRENCY
 from vistafetch.model import Fund
 from vistafetch.model.asset import PriceData
 from vistafetch.model.asset.financial_asset import FinancialAsset
@@ -78,8 +78,8 @@ class TestFinancialAsset(TestCase):
         result = Fund.model_validate(test_input).price_data
 
         self.assertTrue(isinstance(result, PriceData))
-        self.assertEqual("EUR", result.currency_symbol)
-        self.assertEqual(7.09, result.last)
+        self.assertEqual(EXPECTED_PRICE_DATA_CURRENCY, result.currency_symbol)
+        self.assertEqual(EXPECTED_PRICE_DATA_LAST, result.last)
         self.assertEqual(
             datetime(
                 year=2023,
@@ -93,3 +93,20 @@ class TestFinancialAsset(TestCase):
             ),
             result.datetime_open,
         )
+
+    @patch(
+        "vistafetch.model.asset.financial_asset.api_session.get",
+        side_effect=mock_api_call,
+    )
+    def test_price_data_get_latest_price_data(self, session_mock: MagicMock):
+        test_input = {
+            "displayType": "fund",
+            "entityType": "FUND",
+            "isin": "DE00000000",
+            "name": "demon",
+            "symbol": "TEST",
+            "tinyName": "demon",
+            "wkn": "TEST00",
+        }
+
+        Fund.model_validate(test_input).get_latest_price_data()
--- tests/model/asset/test_financial_data.py
+++ tests/model/asset/test_financial_data.py
@@ -1,31 +1,20 @@
 from datetime import timezone
 from unittest import TestCase
 
+from tests.test_utils.requests_utils import EXPECTED_PRICE_DATA
 from vistafetch.model.asset import PriceData
 
 
 class TestPriceData(TestCase):
-    test_input = {
-        "isoCurrency": "EUR",
-        "open": 7.045,
-        "low": 7.02,
-        "datetimeOpen": "2023-08-25T07:00:21.999",
-        "last": 7.09,
-        "addendum": "",
-        "datetimeHigh": "2023-08-25T10:11:52.000",
-        "datetimeLow": "2023-08-25T15:07:10.000+00:00",
-        "high": 7.135,
-        "datetimeLast": "2023-08-25T15:35:10.000+00:00",
-    }
 
     def test_datetime_validator_add_timezone(self):
-        result = PriceData.model_validate(self.test_input)
+        result = PriceData.model_validate(EXPECTED_PRICE_DATA)
 
         self.assertEqual(timezone.utc, result.datetime_open.tzinfo)
         self.assertEqual(timezone.utc, result.datetime_last.tzinfo)
 
     def test_datetime_validator_no_timestamp(self):
-        test_input_malformed = self.test_input.copy()
+        test_input_malformed = EXPECTED_PRICE_DATA
         test_input_malformed["datetimeOpen"] = "not-a-timestamp"
 
         with self.assertRaises(ValueError):
